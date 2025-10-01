cb0bde1c52c3ed0d8299190db1c4472b5b7c5b9a
Sh@dowRig <97234474+ACSiskin@users.noreply.github.com>
2025-10-01 21:34:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // UWAGA: jeśli to Project Pages (np. user.github.io/repo-name), ustaw base:
 export default defineConfig({
   plugins: [react()],
-  base: '/<NAZWA_REPO>/', // <- podmień; dla user/org pages usuń tę linię
+  base: '/reconica-reconica.dev/', // <- podmień; dla user/org pages usuń tę linię
 })
